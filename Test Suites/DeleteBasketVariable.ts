590d5c4367e9d1614fd5584739656a91bd8ebfee
DéboraFalcão <DboraFalco@SBS-DEBFAL>
2023-01-19 16:11:59 +0000

    C1

--- Test Suites/DeleteBasketVariable.ts
+++ Test Suites/DeleteBasketVariable.ts
@@ -11,26 +11,4 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>03f75de2-82ce-48be-b9dc-75f041e06c0c</testSuiteGuid>
-   <testCaseLink>
-      <guid>7dd5c978-5eb5-46d4-8a03-79c89473a3af</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/DeleteBasket2</testCaseId>
-      <testDataLink>
-         <combinationType>ONE</combinationType>
-         <id>f677f003-04bc-493a-a4cd-71e3e86a06e9</id>
-         <iterationEntity>
-            <iterationType>ALL</iterationType>
-            <value></value>
-         </iterationEntity>
-         <testDataId>Data Files/languages</testDataId>
-      </testDataLink>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId>f677f003-04bc-493a-a4cd-71e3e86a06e9</testDataLinkId>
-         <type>DATA_COLUMN</type>
-         <value>locale</value>
-         <variableId>e61f862a-4387-4902-b953-4802288da002</variableId>
-      </variableLink>
-   </testCaseLink>
 </TestSuiteEntity>
