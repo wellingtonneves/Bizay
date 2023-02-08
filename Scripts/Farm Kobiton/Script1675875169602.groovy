764f0d4913ef390914c2fcf35e57b92b93b8c4b1
DéboraFalcão <DboraFalco@SBS-DEBFAL>
2023-02-08 17:20:13 +0000

    Kobiton Farm

--- Scripts/Farm Kobiton/Script1675875169602.groovy
+++ Scripts/Farm Kobiton/Script1675875169602.groovy
@@ -15,7 +15,7 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
-
+import org.openqa.selenium.Keys as Keys
 
 import org.openqa.selenium.remote.DesiredCapabilities
 import com.kms.katalon.core.appium.driver.AppiumDriverManager
@@ -24,7 +24,6 @@ import com.kms.katalon.core.mobile.driver.MobileDriverType
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.util.internal.PathUtil as PathUtil
 import io.appium.java_client.android.AndroidDriver
-
 
 String kobitonServerUrl = "https://debora.falcao:4cd66388-16e2-43ac-8bad-272b8edb65f8@api.kobiton.com/wd/hub";
 
@@ -41,22 +40,14 @@ capabilities.setCapability("deviceGroup", "KOBITON");
 // For deviceName, platformVersion Kobiton supports wildcard
 // character *, with 3 formats: *text, text* and *text*
 // If there is no *, Kobiton will match the exact text provided
-capabilities.setCapability("deviceName", "Galaxy Note20 5G");
+capabilities.setCapability("deviceName", "Galaxy S10");
 capabilities.setCapability("platformVersion", "12");
 capabilities.setCapability("platformName", "Android"); 
-//capabilities.setCapability("app", "kobiton-store:496100")
+capabilities.setCapability("app", "kobiton-store:496238")
 
-WebUI.openBrowser(openbrowser)
+//Mobile.startApplication('kobiton-store:496238', true)
 
-WebUI.click(findTestObject(entry))
+Mobile.tap(findTestObject('Object Repository/Farm Kobiton/android.widget.TextView - Me'), 0)
 
-WebUI.callTestCase(findTestCase('Suporte/UserPdw'), [:], FailureHandling.STOP_ON_FAILURE)
-
-WebUI.click(findTestObject(start))
-
-WebUI.click(findTestObject(myaccount))
-
-WebUI.click(findTestObject(exit))
-
-WebUI.closeBrowser()
+Mobile.closeApplication()
 
