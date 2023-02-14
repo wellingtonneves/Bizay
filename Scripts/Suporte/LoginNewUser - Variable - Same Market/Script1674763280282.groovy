ecfb96ce31f2758b33fc5c82c6e512c7d2cc2341
DéboraFalcão <DboraFalco@SBS-DEBFAL.Home>
2023-02-14 14:46:05 +0000

    Ajustes

--- Scripts/Suporte/LoginNewUser - Variable - Same Market/Script1674763280282.groovy
+++ Scripts/Suporte/LoginNewUser - Variable - Same Market/Script1674763280282.groovy
@@ -17,35 +17,6 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
 
-import org.openqa.selenium.remote.DesiredCapabilities
-import com.kms.katalon.core.appium.driver.AppiumDriverManager
-import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
-import com.kms.katalon.core.mobile.driver.MobileDriverType
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.util.internal.PathUtil as PathUtil
-import io.appium.java_client.android.AndroidDriver
-
-
-String kobitonServerUrl = "https://debora.falcao:4cd66388-16e2-43ac-8bad-272b8edb65f8@api.kobiton.com/wd/hub";
-
-DesiredCapabilities capabilities = new DesiredCapabilities();
-// The generated session will be visible to you only. 
-capabilities.setCapability("sessionName", "Automation test session");
-capabilities.setCapability("sessionDescription", "");
-capabilities.setCapability("deviceOrientation", "portrait");
-capabilities.setCapability("captureScreenshots", true);
-capabilities.setCapability("useConfiguration", "");
-capabilities.setCapability("autoWebview", true);
-capabilities.setCapability("browserName", "chrome");
-capabilities.setCapability("deviceGroup", "KOBITON");
-// For deviceName, platformVersion Kobiton supports wildcard
-// character *, with 3 formats: *text, text* and *text*
-// If there is no *, Kobiton will match the exact text provided
-capabilities.setCapability("deviceName", "Galaxy Note20 5G");
-capabilities.setCapability("platformVersion", "12");
-capabilities.setCapability("platformName", "Android"); 
-//capabilities.setCapability("app", "kobiton-store:496100")
-
 WebUI.openBrowser(openbrowser)
 
 WebUI.click(findTestObject(entry))
