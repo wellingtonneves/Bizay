eab2c7424d6635661eba4f95e293c383860dd495
DéboraFalcão <DboraFalco@SBS-DEBFAL.Home>
2023-02-16 12:07:53 +0000

    ajustes

---
+++ Test Suites/Web Responsive.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Web Responsive</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c95dc1a3-4614-4f93-bd23-ce1bcbacc5cf</testSuiteGuid>
+   <testCaseLink>
+      <guid>865121fc-576e-4420-8e48-946f565984a0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Teste web responsive</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
