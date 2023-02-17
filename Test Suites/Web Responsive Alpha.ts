3b3326fcb0720bb5e0a42f7eaa52bef49f2cf9c8
DéboraFalcão <DboraFalco@SBS-DEBFAL.Home>
2023-02-17 14:57:55 +0000

    Mobile Alpha

---
+++ Test Suites/Web Responsive Alpha.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Web Responsive Alpha</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c95dc1a3-4614-4f93-bd23-ce1bcbacc5cf</testSuiteGuid>
+   <testCaseLink>
+      <guid>8a3265a5-3271-4330-81cb-ec5b786bed52</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/04. Caderno de Testes - Mobile - Alpha/Mobile Alpha</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
