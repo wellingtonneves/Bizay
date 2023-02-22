7d15df949462ecdc084c48f1d03b114c2471c95f
DéboraFalcão <DboraFalco@SBS-DEBFAL.Home>
2023-02-22 17:23:06 +0000

    Teste WEB

---
+++ Test Suites/D.Web Responsive Prod.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>D.Web Responsive Prod</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>dae555f2-0eaa-4217-8e47-46d603b29b04</testSuiteGuid>
+</TestSuiteEntity>
