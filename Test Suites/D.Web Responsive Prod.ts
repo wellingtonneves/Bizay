80609885ebb5c5eb6f48a11f98bf2baf3023f23f
DéboraFalcão <DboraFalco@SBS-DEBFAL.Home>
2023-02-22 19:06:42 +0000

    commit

---
+++ Test Suites/D.Web Responsive Prod.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>D.Web Responsive Prod</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ccdf14db-2a89-4aa9-9b79-7c1388c62013</testSuiteGuid>
+   <testCaseLink>
+      <guid>283a918f-d82b-4cfa-ab78-06befbcbcefb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/04. Caderno de Testes - Mobile - Alpha/CreationNewUserPT - Prod - Web Responsive</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
