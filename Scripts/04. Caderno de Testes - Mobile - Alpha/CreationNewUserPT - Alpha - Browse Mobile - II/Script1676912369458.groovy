dad1cc2733bc0ed31f661ccad93360bca401dce8
WellingtonNeves <WellingtonNeves@host.docker.internal>
2023-02-22 13:56:01 +0000

    Include enhanced click

--- Scripts/04. Caderno de Testes - Mobile - Alpha/CreationNewUserPT - Alpha - Browse Mobile - II/Script1676912369458.groovy
+++ Scripts/04. Caderno de Testes - Mobile - Alpha/CreationNewUserPT - Alpha - Browse Mobile - II/Script1676912369458.groovy
@@ -3,7 +3,7 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+/*import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
@@ -15,23 +15,24 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
+import org.openqa.selenium.Keys as Keys*/ import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
-WebUI.openBrowser('https://www.360imprimir.pt/')
+WebUI.openBrowser('https://alpha.360imprimir.pt/')
 
 WebUI.waitForPageLoad(30)
 
 WebUI.waitForElementVisible(findTestObject('CreatioNewUserPT/Page_360imprimir  Grfica Online, Produtos P_a22487/button_Aceito'), 
-    0)
+    30)
 
 WebUI.verifyElementClickable(findTestObject('CreatioNewUserPT/Page_360imprimir  Grfica Online, Produtos P_a22487/button_Aceito'))
 
-WebUI.click(findTestObject('Object Repository/CreatioNewUserPT/Page_360imprimir  Grfica Online, Produtos P_a22487/button_Aceito'))
+WebUI.enhancedClick(findTestObject('Object Repository/CreatioNewUserPT/Page_360imprimir  Grfica Online, Produtos P_a22487/button_Aceito'))
 
 WebUI.waitForElementVisible(findTestObject('CreatioNewUserPT/Page_360imprimir  Grfica Online, Produtos P_a22487/a_Entrar  Registar'), 
-    0)
+    30)
 
-WebUI.click(findTestObject('Object Repository/CreatioNewUserPT/Page_360imprimir  Grfica Online, Produtos P_a22487/a_Entrar  Registar'))
+WebUI.enhancedClick(findTestObject('Object Repository/CreatioNewUserPT/Page_360imprimir  Grfica Online, Produtos P_a22487/a_Entrar  Registar'))
 
 WebUI.setText(findTestObject('Object Repository/CreatioNewUserPT/Page_Iniciar sesso/input__UserName'), 'sy892syone@gmail.com')
 
@@ -41,23 +42,23 @@ WebUI.setEncryptedText(findTestObject('Object Repository/CreatioNewUserPT/Page_I
 
 WebUI.setEncryptedText(findTestObject('Object Repository/CreatioNewUserPT/Page_Iniciar sesso/input__ConfirmPassword'), 'aeHFOx8jV/A=')
 
-WebUI.click(findTestObject('Object Repository/CreatioNewUserPT/Page_Iniciar sesso/input_Termos e Condies_gpdrCheckbox'))
+WebUI.enhancedClick(findTestObject('Object Repository/CreatioNewUserPT/Page_Iniciar sesso/input_Termos e Condies_gpdrCheckbox'))
 
-WebUI.click(findTestObject('Object Repository/CreatioNewUserPT/Page_Iniciar sesso/div_Criar Conta'))
+WebUI.enhancedClick(findTestObject('Object Repository/CreatioNewUserPT/Page_Iniciar sesso/div_Criar Conta'))
 
-WebUI.click(findTestObject('Object Repository/CreatioNewUserPT/Page_Registar Cliente/i_Particular_icon-ok-sign icon-3x'))
+WebUI.enhancedClick(findTestObject('Object Repository/CreatioNewUserPT/Page_Registar Cliente/i_Particular_icon-ok-sign icon-3x'))
 
 WebUI.setText(findTestObject('Object Repository/CreatioNewUserPT/Page_Registar Cliente/input_Apelido_Name'), 'Debora')
 
 WebUI.setText(findTestObject('Object Repository/CreatioNewUserPT/Page_Registar Cliente/input_Apelido_Surname'), 'Syone')
 
-WebUI.click(findTestObject('Object Repository/CreatioNewUserPT/Page_Registar Cliente/i_Feminino_icon-ok-sign icon-2x'))
+WebUI.enhancedClick(findTestObject('Object Repository/CreatioNewUserPT/Page_Registar Cliente/i_Feminino_icon-ok-sign icon-2x'))
 
 WebUI.setText(findTestObject('Object Repository/CreatioNewUserPT/Page_Registar Cliente/input_Telemvel_Phone'), '961138965')
 
-WebUI.click(findTestObject('Object Repository/CreatioNewUserPT/Page_Registar Cliente/input_Telemvel_createClient'))
+WebUI.enhancedClick(findTestObject('Object Repository/CreatioNewUserPT/Page_Registar Cliente/input_Telemvel_createClient'))
 
-WebUI.click(findTestObject('Object Repository/CreatioNewUserPT/Page_Debora Syone/button_'))
+WebUI.enhancedClick(findTestObject('Object Repository/CreatioNewUserPT/Page_Debora Syone/button_'))
 
 WebUI.closeBrowser()
 
