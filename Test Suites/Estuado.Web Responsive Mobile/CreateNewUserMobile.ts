583a079e56b63d1846c8a6fc3cf3830a091afe81
WellingtonNeves <WellingtonNeves@host.docker.internal>
2023-02-23 10:28:11 +0000

    Include new test suit

---
+++ Test Suites/Estuado.Web Responsive Mobile/CreateNewUserMobile.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CreateNewUserMobile</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>eb94e4f9-9d5c-408b-8d93-500d2064170d</testSuiteGuid>
+   <testCaseLink>
+      <guid>2d0fb388-0ec0-4a6c-a292-c2610b779b47</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/04. Caderno de Testes - Mobile - Alpha/CreationNewUserPT - Alpha - Browse Mobile - II</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
